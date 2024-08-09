0b133dacccf9823ed0fb07b77cf2534a1ab8ccf4
weisman <weisman123@outlook.com>
2024-08-09 13:26:23 -0700

    format code

--- src/pages/drafts.tsx
+++ src/pages/drafts.tsx
@@ -1,79 +1,78 @@
-import React from "react";
-import type { GetStaticProps } from "next";
-import Layout from "../components/Layout";
-import Post, { PostProps } from "../components/Post";
-import { useSession, getSession } from "next-auth/react";
+import React from 'react'
+import type { GetStaticProps } from 'next'
+import Layout from '../components/Layout'
+import Post, { PostProps } from '../components/Post'
+import { useSession, getSession } from 'next-auth/react'
 import prisma from '../lib/prisma'
 
-
 export const getStaticProps: GetStaticProps = async ({ req, res }) => {
-  const session = await getSession({ req });
-  if (!session) {
-    res.statusCode = 403;
-    return { props: { drafts: [] } };
-  }
+    const session = await getSession({ req })
+    if (!session) {
+        res.statusCode = 403
+        return { props: { drafts: [] } }
+    }
 
-  const drafts = await prisma.post.findMany({
-    where: {
-      author: { email: session.user.email },
-      published: false,
-    },
-    include: {
-      author: {
-        select: { name: true },
-      },
-    },
-  });
-  return {
-    props: { drafts },
-    revalidate: 10
-  };
-};
+    const drafts = await prisma.post.findMany({
+        where: {
+            author: { email: session.user.email },
+            published: false,
+        },
+        include: {
+            author: {
+                select: { name: true },
+            },
+        },
+    })
+    return {
+        props: { drafts },
+        revalidate: 10,
+    }
+}
 
 type Props = {
-  drafts: PostProps[];
-};
+    drafts: PostProps[]
+}
 
 const Drafts: React.FC<Props> = (props) => {
-  const {data: session}= useSession();
+    const { data: session } = useSession()
 
-  if (!session) {
+    if (!session) {
+        return (
+            <Layout>
+                <h1>My Drafts</h1>
+                <div>You need to be authenticated to view this page.</div>
+            </Layout>
+        )
+    }
+
     return (
-      <Layout>
-        <h1>My Drafts</h1>
-        <div>You need to be authenticated to view this page.</div>
-      </Layout>
-    );
-  }
-
-  return (
-    <Layout>
-      <div className="page">
-        <h1>My Drafts</h1>
-        <main>
-          {props.drafts.map((post) => (
-            <div key={post.id} className="post">
-              <Post post={post} />
+        <Layout>
+            <div className="page">
+                <h1>My Drafts</h1>
+                <main>
+                    {props.drafts.map((post) => (
+                        <div key={post.id} className="post">
+                            <Post post={post} />
+                        </div>
+                    ))}
+                </main>
             </div>
-          ))}
-        </main>
-      </div>
-      <style jsx>{`
-        .post {
-          background: white;
-          transition: box-shadow 0.1s ease-in;
-        }
+            <style jsx>{`
+                .post {
+                    background: white;
+                    transition: box-shadow 0.1s ease-in;
+                }
 
-        .post:hover {
-          box-shadow: 1px 1px 3px #aaa;
-        }
+                .post:hover {
+                    box-shadow: 1px 1px 3px #aaa;
+                }
 
-        .post + .post {
-          margin-top: 2rem;
-        }
-      `}</style>
-    </Layout>
-  );
-};
+                .post + .post {
+                    margin-top: 2rem;
+                }
+            `}</style>
+        </Layout>
+    )
+}
 
-export default Drafts;
+export default Drafts
