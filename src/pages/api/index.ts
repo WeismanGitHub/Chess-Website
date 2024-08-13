f0ccd6c17423b155903ce253f763271cea004293
weisman <weisman123@outlook.com>
2024-08-13 14:50:06 -0700

    Restructure files

--- src/pages/api/index.ts
+++
@@ -1,5 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-
-export default async function handle(_req: NextApiRequest, res: NextApiResponse) {
-    res.status(200).send({ message: 'hello world!' })
-}
