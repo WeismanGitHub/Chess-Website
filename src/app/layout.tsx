df532da7d3cbeea08ee116cd86228cbcd64e4e90
weisman <weisman123@outlook.com>
2024-08-16 12:36:49 -0700

    Update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
-import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import type { Metadata } from 'next'
 import './css/global.css'
+import React from 'react'
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
