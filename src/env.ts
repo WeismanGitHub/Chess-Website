ff6292aacb21be145fe57279591bbf4115639579
weisman <weisman123@outlook.com>
2024-08-16 12:31:47 -0700

    Load env vars during tests too

--- src/env.ts
+++ src/env.ts
@@ -1,9 +1,14 @@
+import { loadEnvConfig } from '@next/env'
 import zod from 'zod'
+
+loadEnvConfig(process.cwd())
 
 const envSchema = zod.object({
     MONGODB_URI: zod.string(),
 })
 
-export default envSchema.parse({
+const env = envSchema.parse({
     MONGODB_URI: process.env.MONGODB_URI,
 })
+
+export default env
