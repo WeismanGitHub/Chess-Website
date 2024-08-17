207f6e134f409935f926108423d2c8eebb100d32
weisman <weisman123@outlook.com>
2024-08-17 14:45:17 -0700

    Create middleware

---
+++ src/middleware.ts
@@ -0,0 +1,10 @@
+import type { NextRequest } from 'next/server'
+// import { NextResponse } from 'next/server'
+
+export function middleware(request: NextRequest) {
+    console.log(request.url)
+}
+
+export const config = {
+    matcher: '/api/:path*',
+}
