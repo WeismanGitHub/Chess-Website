fb2bb3125a2793fa1b9cc2683bff606199a3aa1e
weisman <weisman123@outlook.com>
2024-08-17 14:00:27 -0700

    Add minlength

--- src/models/user.ts
+++ src/models/user.ts
@@ -18,6 +18,7 @@ const userSchema = new Schema<IUser, UserModel, IUserMethods>(
         name: {
             type: String,
             required: [true, 'Please provide a name.'],
+            minlength: [1, 'Name cannot be less than 1 character.'],
             maxlength: [25, 'Name cannot be more than 25 characters.'],
         },
         password: {
