34ee9589cfffdb1ce7f86e3d9328624beb439419
weisman <weisman123@outlook.com>
2024-08-17 14:19:24 -0700

    Use mongoose-unique-validator

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,4 +1,5 @@
 import { Model, Schema, model, models } from 'mongoose'
+import uniqueValidator from 'mongoose-unique-validator'
 import { compare, hash } from 'bcrypt'
 
 interface IUser {
@@ -20,6 +21,7 @@ const userSchema = new Schema<IUser, UserModel, IUserMethods>(
             required: [true, 'Please provide a name.'],
             minlength: [1, 'Name cannot be less than 1 character.'],
             maxlength: [25, 'Name cannot be more than 25 characters.'],
+            unique: true,
         },
         password: {
             type: String,
@@ -41,5 +43,7 @@ userSchema.pre('save', async function (next) {
 
     next()
 })
+
+userSchema.plugin(uniqueValidator)
 
 export default (models.Users || model<IUser, UserModel>('Users', userSchema)) as UserModel
