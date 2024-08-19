78eb835f0627c571ead07db0e2c650ef940898ea
weisman <weisman123@outlook.com>
2024-08-18 18:20:38 -0700

    Set up boilerplate

--- src/app/api/auth/signup/route.ts
+++ src/app/api/auth/signup/route.ts
@@ -1,6 +1,29 @@
-import { NextApiRequest } from 'next'
+import { credentialsSchema } from '../../../../lib/zod'
+import dbConnect from '../../../../lib/dbConnect'
+import { StatusCodes } from 'http-status-codes'
 import { NextResponse } from 'next/server'
+import User from '../../../../models/User'
 
-export async function POST(req: NextApiRequest) {
-    return NextResponse.json({ message: 'hello world' })
+type body = {
+    name: string | undefined
+    password: string | undefined
+}
+
+export async function POST(req: Request) {
+    const body: body = await req.json().catch(() => {
+        throw new Error('Cannot parse request body.')
+    })
+
+    const { name, password } = await credentialsSchema.parseAsync(body)
+
+    await dbConnect()
+
+    const user = await User.create({
+        name,
+        password,
+    })
+
+    console.log(user)
+
+    return NextResponse.json({}, { status: StatusCodes.CREATED })
 }
