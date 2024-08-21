0bee428fdb0996231f31c92f07b4da3921ae589f
weisman <weisman123@outlook.com>
2024-08-20 21:49:37 -0700

    Change error messages

--- src/lib/zod.ts
+++ src/lib/zod.ts
@@ -3,7 +3,16 @@ import { object, string } from 'zod'
 
 export const credentialsSchema = object({
     name: string({ required_error: 'Name is required' })
-        .min(UserConstants.minNameLength)
-        .max(UserConstants.maxNameLength),
-    password: string({ required_error: 'Password is required' }).min(UserConstants.minPasswordLength),
+        .min(
+            UserConstants.minNameLength,
+            `Name must be more than ${UserConstants.minNameLength} character(s)`
+        )
+        .max(
+            UserConstants.maxNameLength,
+            `Name cannot be more than be more than ${UserConstants.maxNameLength} character(s)`
+        ),
+    password: string({ required_error: 'Password is required' }).min(
+        UserConstants.minPasswordLength,
+        `Password must be more than ${UserConstants.minPasswordLength} character(s)`
+    ),
 })
