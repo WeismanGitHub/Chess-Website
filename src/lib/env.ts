642bb46af1d8aa9e34edf3b2ed5cfae2c0bd3aef
weisman <weisman123@outlook.com>
2024-08-21 21:58:59 -0700

    Add JWT stuff

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -5,10 +5,14 @@ loadEnvConfig(process.cwd())
 
 const envSchema = zod.object({
     MONGODB_URI: zod.string(),
+    JWT_SECRET: zod.string(),
+    JWT_EXPIRATION: zod.string(),
 })
 
 const env = envSchema.parse({
     MONGODB_URI: process.env.MONGODB_URI,
+    JWT_SECRET: process.env.JWT_SECRET,
+    JWT_EXPIRATION: process.env.JWT_EXPIRATION,
 })
 
 export default env
