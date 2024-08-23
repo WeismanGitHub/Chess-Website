f38ab2124147884ddeb0823a51dcb3674af0190b
weisman <weisman123@outlook.com>
2024-08-23 14:24:49 -0700

    Create BotConstants

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -4,3 +4,8 @@ export class UserConstants {
 
     public static readonly minPasswordLength = 10
 }
+
+export class BotConstants {
+    public static readonly minNameLength = 1
+    public static readonly maxNameLength = 25
+}
