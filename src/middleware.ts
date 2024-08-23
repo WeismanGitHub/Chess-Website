2ff0c2c4eac9e58cfa63715eb58c21e7af973c0e
weisman <weisman123@outlook.com>
2024-08-22 20:48:46 -0700

    Add auth middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,12 @@
-import type { NextRequest } from 'next/server'
-// import { NextResponse } from 'next/server'
+import { NextResponse, type MiddlewareConfig, type NextRequest } from 'next/server'
+import { StatusCodes } from 'http-status-codes'
 
 export function middleware(request: NextRequest) {
-    console.log(request.url)
+    if (!request.nextUrl.pathname.startsWith('/api/auth') && !request.cookies.get('auth')) {
+        return new NextResponse('Please sign in.', { status: StatusCodes.UNAUTHORIZED })
+    }
 }
 
-export const config = {
+export const config: MiddlewareConfig = {
     matcher: '/api/:path*',
 }
