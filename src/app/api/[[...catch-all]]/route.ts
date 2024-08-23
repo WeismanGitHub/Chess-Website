17198f171c0b7fedaadbe2813c393f1169fa3d54
weisman <weisman123@outlook.com>
2024-08-23 14:16:17 -0700

    Catch 404s in api

---
+++ src/app/api/[[...catch-all]]/route.ts
@@ -0,0 +1,8 @@
+import { NextRequest, NextResponse } from 'next/server'
+import { StatusCodes } from 'http-status-codes'
+
+export default function handler(_req: NextRequest) {
+    return NextResponse.json({ message: 'This endpoint does not exist.' }, { status: StatusCodes.NOT_FOUND })
+}
+
+export { handler as GET, handler as POST, handler as DELETE, handler as PATCH, handler as PUT }
