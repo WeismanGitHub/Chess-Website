938a254ec2aceaea5a49b0c2929629cf4460a07c
weisman <weisman123@outlook.com>
2024-08-24 15:25:52 -0700

    Create Bot model

---
+++ src/models/Bot.ts
@@ -0,0 +1,29 @@
+import { Model, Schema, model, models } from 'mongoose'
+import { BotConstants } from '../lib/constants'
+
+interface IBot {
+    name: string
+}
+
+type BotModel = Model<IBot, {}>
+
+const botSchema = new Schema<IBot, BotModel>(
+    {
+        name: {
+            type: String,
+            required: [true, 'Please provide a name.'],
+            minlength: [
+                BotConstants.minNameLength,
+                `Name cannot be less than ${BotConstants.minNameLength} character(s).`,
+            ],
+            maxlength: [
+                BotConstants.maxNameLength,
+                `Name cannot be more than ${BotConstants.maxNameLength} characters.`,
+            ],
+            unique: true,
+        },
+    },
+    { timestamps: true }
+)
+
+export default (models.Users || model<IBot, BotModel>('Bots', botSchema)) as BotModel
