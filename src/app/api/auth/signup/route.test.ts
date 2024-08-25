fa542ceeaccbeaeb4c461c8f85f02c3779966b3a
weisman <weisman123@outlook.com>
2024-08-24 20:47:31 -0700

    Create tests for signup endpoint

--- src/app/api/auth/signup/route.test.ts
+++ src/app/api/auth/signup/route.test.ts
@@ -1,8 +1,54 @@
-import { POST } from './route'
+import { testApiHandler } from 'next-test-api-route-handler'
+import dbConnect from '../../../../lib/dbConnect'
+import { StatusCodes } from 'http-status-codes'
+import { User } from '../../../../models'
+import * as appHandler from './route'
+import mongoose from 'mongoose'
 
-it('should return data with status 200', async () => {
-    const response = await POST()
-    const body = await response.json()
+beforeAll(async () => {
+    await dbConnect()
+})
 
-    expect(body.message).toBe('hello world')
+afterEach(async () => {
+    await User.deleteMany()
+})
+
+afterAll(async () => {
+    await mongoose.connection.close()
+})
+
+const validCredentials = {
+    name: '1',
+    password: 'HelloWorld1',
+}
+
+describe('POST /api/auth/signup', () => {
+    it('should return status 201 and auth cookie', async () => {
+        await testApiHandler({
+            appHandler,
+            async test({ fetch }) {
+                const res = await fetch({
+                    method: 'POST',
+                    body: JSON.stringify(validCredentials),
+                })
+
+                expect(res.status).toBe(StatusCodes.CREATED)
+                expect(res.cookies.find((cookie) => cookie['auth'])).toBeTruthy()
+            },
+        })
+    })
+
+    it('should return status 400 for invalid input', async () => {
+        await testApiHandler({
+            appHandler,
+            async test({ fetch }) {
+                const res = await fetch({
+                    method: 'POST',
+                    body: JSON.stringify({}),
+                })
+
+                expect(res.status).toBe(StatusCodes.BAD_REQUEST)
+            },
+        })
+    })
 })
