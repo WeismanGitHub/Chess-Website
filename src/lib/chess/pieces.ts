41f0942a18467a5bd268d0c11168c7a33b8c86b4
weisman <weisman123@outlook.com>
2024-09-05 17:01:18 -0700

    Setup pieces classes

---
+++ src/lib/chess/pieces.ts
@@ -0,0 +1,50 @@
+import Board from './board'
+
+export abstract class Piece {
+    public white: boolean
+    public killed: boolean = true
+
+    constructor(white: boolean) {
+        this.white = white
+    }
+
+    abstract canMove(board: Board, x: number, y: number): boolean
+}
+
+export class King extends Piece {
+    public canCastle = true
+
+    canMove(board: Board, x: number, y: number): boolean {
+        throw new Error('Method not implemented.')
+    }
+}
+
+export class Queen extends Piece {
+    canMove(board: Board, x: number, y: number): boolean {
+        throw new Error('Method not implemented.')
+    }
+}
+
+export class Bishop extends Piece {
+    canMove(board: Board, x: number, y: number): boolean {
+        throw new Error('Method not implemented.')
+    }
+}
+
+export class Knight extends Piece {
+    canMove(board: Board, x: number, y: number): boolean {
+        throw new Error('Method not implemented.')
+    }
+}
+
+export class Rook extends Piece {
+    canMove(board: Board, x: number, y: number): boolean {
+        throw new Error('Method not implemented.')
+    }
+}
+
+export class Pawn extends Piece {
+    canMove(board: Board, x: number, y: number): boolean {
+        throw new Error('Method not implemented.')
+    }
+}
