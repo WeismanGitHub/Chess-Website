a4baebcc92ab6263e5885b0c6dd9715c0d2e271c
weisman <weisman123@outlook.com>
2024-09-09 18:22:00 -0700

    Update boilerplate canMove method

--- src/lib/chess/pieces.ts
+++ src/lib/chess/pieces.ts
@@ -1,51 +1,51 @@
+import Square from './square'
 import Board from './board'
 
 export abstract class Piece {
-    public white: boolean
-    public dead: boolean
+    public color: Color
+    public dead: boolean = false
 
-    constructor(white: boolean = true, dead: boolean = false) {
-        this.white = white
-        this.dead = dead
+    constructor(color: Color = 'white') {
+        this.color = color
     }
 
-    abstract canMove(board: Board, x: number, y: number): boolean
+    abstract canMove(board: Board, start: Square, end: Square): boolean
 }
 
 export class King extends Piece {
     public canCastle = true
 
-    canMove(board: Board, x: number, y: number): boolean {
+    canMove(board: Board, start: Square, end: Square): boolean {
         throw new Error('Method not implemented.')
     }
 }
 
 export class Queen extends Piece {
-    canMove(board: Board, x: number, y: number): boolean {
+    canMove(board: Board, start: Square, end: Square): boolean {
         throw new Error('Method not implemented.')
     }
 }
 
 export class Bishop extends Piece {
-    canMove(board: Board, x: number, y: number): boolean {
+    canMove(board: Board, start: Square, end: Square): boolean {
         throw new Error('Method not implemented.')
     }
 }
 
 export class Knight extends Piece {
-    canMove(board: Board, x: number, y: number): boolean {
+    canMove(board: Board, start: Square, end: Square): boolean {
         throw new Error('Method not implemented.')
     }
 }
 
 export class Rook extends Piece {
-    canMove(board: Board, x: number, y: number): boolean {
+    canMove(board: Board, start: Square, end: Square): boolean {
         throw new Error('Method not implemented.')
     }
 }
 
 export class Pawn extends Piece {
-    canMove(board: Board, x: number, y: number): boolean {
+    canMove(board: Board, start: Square, end: Square): boolean {
         throw new Error('Method not implemented.')
     }
 }
