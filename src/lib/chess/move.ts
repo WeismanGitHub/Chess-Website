14ff83296ac7525a9dc7fef91c57b71636a30480
weisman <weisman123@outlook.com>
2024-09-09 18:20:54 -0700

    Add necessary info to Move class

--- src/lib/chess/move.ts
+++ src/lib/chess/move.ts
@@ -1,9 +1,37 @@
-export default class Move {
-    public white: boolean
-    public isCastlingMove: boolean
+import { Piece } from './pieces'
+import Square from './square'
 
-    constructor(white: boolean, isCastlingMove = false) {
-        this.white = white
-        this.isCastlingMove = isCastlingMove
+export default class Move {
+    public color: Color
+    public pieceMoved: Piece
+    public pieceKilled: Piece | null
+
+    public start: Square
+    public end: Square
+
+    public castlingMove: boolean
+    public check: boolean
+    public checkmate: boolean
+
+    constructor(
+        piece: Piece,
+        pieceKilled: Piece | null,
+        color: Color,
+        start: Square,
+        end: Square,
+        check: boolean = false,
+        castlingMove: boolean = false,
+        checkmate: boolean = false
+    ) {
+        this.color = color
+        this.pieceMoved = piece
+        this.pieceKilled = pieceKilled
+
+        this.start = start
+        this.end = end
+
+        this.castlingMove = castlingMove
+        this.check = check
+        this.checkmate = checkmate
     }
 }
