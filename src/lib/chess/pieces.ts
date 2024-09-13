960e0dc534da6c0584876561c64c3d01af66e839
weisman <weisman123@outlook.com>
2024-09-12 17:23:01 -0700

    Implement universalCheck decorator for canMove

--- src/lib/chess/pieces.ts
+++ src/lib/chess/pieces.ts
@@ -1,9 +1,24 @@
 import Square from './square'
 import Board from './board'
 
+function universalCheck(_target: Object, _propertyKey: string, descriptor: TypedPropertyDescriptor<any>) {
+    const originalMethod = descriptor.value
+
+    descriptor.value = function (board: Board, start: Square, end: Square) {
+        if (!start.piece) return false
+
+        if (start.piece.color === end.piece?.color) return false
+
+        const result = originalMethod.apply(this, board, start, end)
+
+        return result
+    }
+
+    return descriptor
+}
+
 export abstract class Piece {
     public color: Color
-    public dead: boolean = false
 
     constructor(color: Color = 'white') {
         this.color = color
@@ -15,37 +30,43 @@ export abstract class Piece {
 export class King extends Piece {
     public canCastle = true
 
-    canMove(board: Board, start: Square, end: Square): boolean {
+    @universalCheck
+    canMove(_board: Board, _start: Square, _end: Square): boolean {
         throw new Error('Method not implemented.')
     }
 }
 
 export class Queen extends Piece {
-    canMove(board: Board, start: Square, end: Square): boolean {
+    @universalCheck
+    canMove(_board: Board, _start: Square, _end: Square): boolean {
         throw new Error('Method not implemented.')
     }
 }
 
 export class Bishop extends Piece {
-    canMove(board: Board, start: Square, end: Square): boolean {
+    @universalCheck
+    canMove(_board: Board, _start: Square, _end: Square): boolean {
         throw new Error('Method not implemented.')
     }
 }
 
 export class Knight extends Piece {
-    canMove(board: Board, start: Square, end: Square): boolean {
+    @universalCheck
+    canMove(_board: Board, _start: Square, _end: Square): boolean {
         throw new Error('Method not implemented.')
     }
 }
 
 export class Rook extends Piece {
-    canMove(board: Board, start: Square, end: Square): boolean {
+    @universalCheck
+    canMove(_board: Board, _start: Square, _end: Square): boolean {
         throw new Error('Method not implemented.')
     }
 }
 
 export class Pawn extends Piece {
-    canMove(board: Board, start: Square, end: Square): boolean {
+    @universalCheck
+    canMove(_board: Board, _start: Square, _end: Square): boolean {
         throw new Error('Method not implemented.')
     }
 }
