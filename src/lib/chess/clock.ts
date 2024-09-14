cd411a69fbb82cfc04e2571b1252228a4b3bf16f
weisman <weisman123@outlook.com>
2024-09-14 11:05:47 -0700

    Create chess clock

---
+++ src/lib/chess/clock.ts
@@ -0,0 +1,48 @@
+class Clock {
+    public whiteSeconds: number
+    public blackSeconds: number
+
+    public current: 'black' | 'white' = 'white'
+
+    private interval: NodeJS.Timer | null = null
+
+    constructor(seconds: number) {
+        if (seconds <= 0) {
+            throw new Error(`${seconds} is an invalid amount of time.`)
+        }
+
+        this.whiteSeconds = seconds
+        this.blackSeconds = seconds
+    }
+
+    public start() {
+        if (this.interval) {
+            throw new Error('Clock already started.')
+        }
+
+        this.interval = setInterval(() => {
+            if ((this.whiteSeconds === 0 || this.blackSeconds === 0) && this.interval) {
+                return clearInterval(this.interval)
+            }
+
+            if (this.current === 'white') {
+                this.whiteSeconds -= 1
+            } else {
+                this.blackSeconds -= 1
+            }
+        }, 1000)
+    }
+
+    public switch() {
+        this.current = this.current === 'white' ? 'black' : 'white'
+    }
+}
+
+const clock = new Clock(10)
+clock.start()
+
+setTimeout(() => {
+    clock.switch()
+}, 5000)
+
+export {}
