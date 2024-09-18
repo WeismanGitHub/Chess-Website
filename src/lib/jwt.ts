ea0d24b8934e734cfdb0089646a71d8ac4de5c97
weisman <weisman123@outlook.com>
2024-09-17 21:10:07 -0700

    Create separate function for just decoding a jwt

--- src/lib/jwt.ts
+++ src/lib/jwt.ts
@@ -8,13 +8,24 @@ export function signAuthJwt(userId: string) {
     return jwt.sign({ userId }, env.JWT_SECRET, { expiresIn: env.JWT_EXPIRATION })
 }
 
-export function decodeAuthJwt(request: NextRequest): string {
+export function getUserId(request: NextRequest): string {
     const value = request.cookies.get('auth')?.value
 
     if (!value) {
         throw new ApiError(StatusCodes.INTERNAL_SERVER_ERROR, 'Could not read auth cookie.')
     }
 
+    // @ts-ignore
+    const userId = jwt.decode(value)?.userId
+
+    if (!userId) {
+        throw new ApiError(StatusCodes.INTERNAL_SERVER_ERROR, 'Could not decode auth jwt.')
+    }
+
+    return userId
+}
+
+export function decodeAuthJwt(value: string): string {
     // @ts-ignore
     const userId = jwt.decode(value)?.userId
 
