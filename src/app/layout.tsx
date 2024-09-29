78acb08ccd9003232d33d91b8df6ae0765c1d61d
weisman <weisman123@outlook.com>
2024-09-29 14:27:02 -0700

    Add background

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,12 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body className={inter.className}>{children}</body>
+            <body>
+                {/* https://github.com/ibelick/background-snippets */}
+                <div className="absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
+                    <div className={inter.className}>{children}</div>
+                </div>
+            </body>
         </html>
     )
 }
