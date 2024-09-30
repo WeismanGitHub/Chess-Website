1e5f94fa3ca963a592912141c4c7b43677ed00d2
weisman <weisman123@outlook.com>
2024-09-30 14:29:53 -0700

    Customize not-found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,18 @@
-export default function NotFound() {
-    return <div>Not Found</div>;
+'use client'
+
+import { Button } from 'flowbite-react'
+import Link from 'next/link'
+import React from 'react'
+
+export default function () {
+    return (
+        <div className="flex flex-col items-center gap-4">
+            <span className="w-full text-6xl font-bold text-black">404 Not Found</span>
+            <Button>
+                <Link href="/" className="w-fit">
+                    Return to home
+                </Link>
+            </Button>
+        </div>
+    )
 }
