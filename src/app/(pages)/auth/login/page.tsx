3031d31cfd5889b955ade0fea72a03b698e92719
weisman <weisman123@outlook.com>
2024-09-30 13:44:25 -0700

    Finish login page html/css

--- src/app/(pages)/auth/login/page.tsx
+++ src/app/(pages)/auth/login/page.tsx
@@ -3,18 +3,18 @@
 import { Button, Label, TextInput } from 'flowbite-react'
 import React from 'react'
 
-export default function Auth() {
+export default function () {
     return (
         <>
             <div className="mx-auto flex w-full flex-col items-center justify-center px-6 py-8 lg:py-0">
-                <a href="#" className="mb-6 flex items-center text-4xl font-semibold">
+                <a href="/" className="mb-6 flex items-center text-4xl font-semibold">
                     <img className="h-12 w-12" src="/icon.svg" alt="pawn logo" />
                     Chess
                 </a>
                 <div className="w-full rounded-lg bg-white shadow sm:max-w-md md:mt-0 xl:p-0">
                     <div className="space-y-4 p-6 sm:p-8 md:space-y-6">
                         <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
-                            Create an account
+                            Log in
                         </h1>
                         <form className="space-y-4 md:space-y-6" action="#">
                             <div>
@@ -47,32 +47,16 @@ export default function Auth() {
                                     required={true}
                                 />
                             </div>
-                            <div>
-                                <label
-                                    htmlFor="confirm-password"
-                                    className="mb-2 block text-sm font-medium text-gray-900"
-                                >
-                                    Confirm password
-                                </label>
-                                <TextInput
-                                    type="text"
-                                    name="confirm-password"
-                                    id="confirm-password"
-                                    placeholder="••••••••••"
-                                    required={true}
-                                    className="focus:border-none"
-                                />
-                            </div>
                             <Button
                                 type="submit"
                                 className="text-md w-full rounded-lg px-5 py-2.5 text-center focus:outline-none focus:ring-4"
                             >
-                                Create an account
+                                Log in
                             </Button>
                             <p className="text-sm font-light text-gray-500">
-                                Already have an account?{' '}
-                                <a href="#" className="text-primary-600 font-medium hover:underline">
-                                    Login here
+                                Don't have an account?{' '}
+                                <a href="/auth/register" className="font-medium hover:underline">
+                                    Register here
                                 </a>
                             </p>
                         </form>
