b6e01d3670067bf5dc673d9cc7d8ee52a5e6b7b6
weisman <weisman123@outlook.com>
2024-09-30 13:23:19 -0700

    Customize navbar

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -2,27 +2,28 @@
 
 import Link from 'next/link'
 import { Navbar } from 'flowbite-react'
+import { usePathname } from 'next/navigation'
 
 export default function () {
+    const path = usePathname()
+
     return (
         <Navbar fluid rounded>
-            <Navbar.Brand as={Link} href="https://flowbite-react.com">
-                <img src="/favicon.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite React Logo" />
-                <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-                    Flowbite React
-                </span>
+            <Navbar.Brand as={Link} href="/">
+                <img src="/icon.svg" className="mr-3 h-6 sm:h-9" alt="pawn logo" />
+                <span className="self-center whitespace-nowrap text-xl font-semibold">Chess</span>
             </Navbar.Brand>
             <Navbar.Toggle />
             <Navbar.Collapse>
-                <Navbar.Link href="#" active>
+                <Navbar.Link href="/" active={path == '/'}>
                     Home
                 </Navbar.Link>
-                <Navbar.Link as={Link} href="#">
+                <Navbar.Link as={Link} href="/about" active={path == '/about'}>
                     About
                 </Navbar.Link>
-                <Navbar.Link href="#">Services</Navbar.Link>
-                <Navbar.Link href="#">Pricing</Navbar.Link>
-                <Navbar.Link href="#">Contact</Navbar.Link>
+                <Navbar.Link href="/auth/register" active={path == '/auth/register'}>
+                    Register
+                </Navbar.Link>
             </Navbar.Collapse>
         </Navbar>
     )
