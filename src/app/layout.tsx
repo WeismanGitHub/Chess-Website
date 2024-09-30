a8f390ed0b2a5413840600cfb7686c6b2ea9980a
weisman <weisman123@outlook.com>
2024-09-30 12:05:31 -0700

    Finish layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
+import Navbar from './components/navbar'
 import { Inter } from 'next/font/google'
 import type { Metadata } from 'next'
 import React from 'react'
 import './global.css'
-
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -18,9 +18,14 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body>
-                {/* https://github.com/ibelick/background-snippets */}
-                <div className="absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
-                    <div className={inter.className}>{children}</div>
+                {/* Background */}
+                <div className="absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]" />
+
+                <div className="flex min-h-screen flex-col">
+                    <Navbar />
+                    <div className={`${inter.className} flex flex-1 items-center justify-center`}>
+                        {children}
+                    </div>
                 </div>
             </body>
         </html>
