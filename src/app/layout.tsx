858a42dfb1d4bbdd7206aac4baf23b25fba70480
weisman <weisman123@outlook.com>
2024-09-30 13:37:52 -0700

    Change background color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,9 +18,8 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body>
-                {/* Background */}
-                <div className="absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]" />
-
+                {/* https://github.com/ibelick/background-snippets */}
+                <div className="absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(14,116,144,0.25),rgba(255,255,255,0))]" />
                 <div className="flex min-h-screen flex-col">
                     <Navbar />
                     <div className={`${inter.className} flex flex-1 items-center justify-center`}>
