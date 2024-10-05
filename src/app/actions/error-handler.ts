2b9e3aed9c2bbab16747ecbe6b1a48da012e8b92
weisman <weisman123@outlook.com>
2024-10-04 21:39:23 -0700

    Create errorHandler

---
+++ src/app/actions/error-handler.ts
@@ -0,0 +1,41 @@
+import { ApiError } from 'next/dist/server/api-utils'
+import { ZodError } from 'zod'
+
+export default function errorHandler<argument, result>(action: (arg: argument) => Promise<result>) {
+    return async function (
+        arg: argument
+    ): Promise<{ success: true; result: result } | { success: false; message: string }> {
+        try {
+            const result = await action(arg)
+
+            return {
+                success: true,
+                result,
+            }
+        } catch (error) {
+            if (error instanceof ApiError) {
+                return {
+                    success: false,
+                    message: error.message,
+                }
+            }
+
+            if (error instanceof ZodError) {
+                let message = ''
+
+                for (const err of error.errors) {
+                    message += err.message + '. '
+                }
+
+                return {
+                    success: false,
+                    message: message.trimEnd(),
+                }
+            }
+
+            console.error(error)
+
+            return { success: false, message: 'Something went wrong.' }
+        }
+    }
+}
