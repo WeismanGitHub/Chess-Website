77648b359822ab0dee0585ad40983cd05c90a4bf
weisman <weisman123@outlook.com>
2024-10-07 15:48:57 -0700

    Set http headers

---
+++ next.config.js
@@ -0,0 +1,44 @@
+// @ts-check
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    async headers() {
+        return [
+            {
+                source: '/:path*',
+                headers: [
+                    {
+                        key: 'X-Content-Type-Options',
+                        value: 'nosniff',
+                    },
+                    {
+                        key: 'Referrer-Policy',
+                        value: 'strict-origin-when-cross-origin',
+                    },
+                    {
+                        key: 'Strict-Transport-Security',
+                        value: 'max-age=63072000; includeSubDomains; preload',
+                    },
+                    {
+                        key: 'Cross-Origin-Opener-Policy',
+                        value: 'same-origin',
+                    },
+                    {
+                        key: 'Cross-Origin-Embedder-Policy',
+                        value: 'require-corp',
+                    },
+                    {
+                        key: 'Cross-Origin-Resource-Policy',
+                        value: 'same-site',
+                    },
+                    {
+                        key: 'Permissions-Policy',
+                        value: 'geolocation=(), camera=(), microphone=()',
+                    },
+                ],
+            },
+        ]
+    },
+}
+
+module.exports = nextConfig
