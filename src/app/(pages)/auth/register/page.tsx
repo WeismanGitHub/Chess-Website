9a7db25c15dcbe665aa7d589dea759b4c13f370f
weisman <weisman123@outlook.com>
2024-10-07 14:11:19 -0700

    Finish register page

--- src/app/(pages)/auth/register/page.tsx
+++ src/app/(pages)/auth/register/page.tsx
@@ -1,19 +1,31 @@
 'use client'
 
 import { Button, Label, TextInput } from 'flowbite-react'
+import { FailureToast } from '../../../components/toasts'
 import { credentialsSchema } from '../../../../lib/zod'
+import { register } from '../../../actions/auth'
+import { useRouter } from 'next/navigation'
+import React, { useState } from 'react'
 import { Formik, Form } from 'formik'
-import React from 'react'
 
 export default function () {
+    const [message, setMessage] = useState<string | null>(null)
+    const router = useRouter()
+
     return (
         <>
+            <FailureToast
+                message={message ?? ''}
+                show={message !== null}
+                handleDismiss={() => setMessage(null)}
+            />
+
             <div className="mx-auto flex w-full flex-col items-center justify-center px-6 py-8 lg:py-0">
                 <a href="/" className="mb-6 flex items-center text-4xl font-semibold">
                     <img className="h-12 w-12" src="/icon.svg" alt="pawn logo" />
                     Chess
                 </a>
-                <div className="w-full rounded-lg bg-white shadow sm:max-w-md md:mt-0 xl:p-0">
+                <div className="w-full rounded-lg bg-white shadow sm:max-w-md md:mt-0 lg:m-5 xl:p-0">
                     <div className="space-y-4 p-6 sm:p-8 md:space-y-6">
                         <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
                             Create an account
@@ -46,9 +58,16 @@ export default function () {
 
                                 return errors
                             }}
-                            onSubmit={(values) => {
-                                console.log(values)
-                                throw new Error('Function not implemented.')
+                            onSubmit={async (values) => {
+                                setMessage(null)
+
+                                const res = await register(values)
+
+                                if (res.success) {
+                                    return router.push('/')
+                                }
+
+                                setMessage(res.message)
                             }}
                             validateOnChange={true}
                             validateOnBlur={true}
