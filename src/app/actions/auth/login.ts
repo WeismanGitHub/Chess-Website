d943572369821c61aee7409c725daceed8e104f3
weisman <weisman123@outlook.com>
2024-10-07 14:58:36 -0700

    Create login action

--- src/app/actions/auth/login.ts
+++ src/app/actions/auth/login.ts
@@ -1,3 +1,27 @@
 'use server'
 
-export default async function () {}
+import { credentialsSchema } from '../../../lib/zod'
+import CustomError from '../../../lib/custom-error'
+import dbConnect from '../../../lib/db-connect'
+import { signAuthJwt } from '../../../lib/jwt'
+import errorHandler from '../error-handler'
+import { cookies } from 'next/headers'
+import { User } from '../../../models'
+
+export default errorHandler(async (body: { name: string; password: string }) => {
+    const { name, password } = await credentialsSchema.parseAsync(body)
+
+    await dbConnect()
+
+    const user = await User.findOne({ name })
+
+    if (!user) {
+        throw new CustomError('Could not find an account with that name.')
+    }
+
+    if (!user.isValidPassword(password)) {
+        throw new CustomError('That password is invalid.')
+    }
+
+    cookies().set('auth', signAuthJwt(user.id), { httpOnly: true, sameSite: true, secure: true })
+})
