418621f9fc8d84123754a12754167a8199e57dac
weisman <weisman123@outlook.com>
2024-10-07 16:53:26 -0700

    Remove comment

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -15,7 +15,6 @@ export default function () {
             </Navbar.Brand>
             <Navbar.Toggle />
             <Navbar.Collapse>
-                {/* make the hitbox larger */}
                 <Navbar.Link className="nav-link" as={Link} href="/" active={path == '/'}>
                     Home
                 </Navbar.Link>
