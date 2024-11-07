2157f0cdc844f193cf2b372178827108639a478c
weisman <weisman123@outlook.com>
2024-11-06 16:37:58 -0800

    Implement send message component

--- src/app/(pages)/games/sidebar.tsx
+++ src/app/(pages)/games/sidebar.tsx
@@ -1,11 +1,34 @@
 'use client'
 
-import React, { ReactNode, useEffect, useRef, useState } from 'react'
+import React, { Dispatch, ReactNode, SetStateAction, useEffect, useRef, useState } from 'react'
 import { DefaultEventsMap } from 'socket.io/dist/typed-events'
+import { Button, TextInput } from 'flowbite-react'
 import { Socket } from 'socket.io-client'
 
-export default function Chat({ socket }: { socket: Socket<DefaultEventsMap, DefaultEventsMap> }) {
+function Flag() {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="100%"
+            height="100%"
+            fill="currentColor"
+            className="fill-teal-700"
+            viewBox="0 0 16 16"
+        >
+            <path d="M14.778.085A.5.5 0 0 1 15 .5V8a.5.5 0 0 1-.314.464L14.5 8l.186.464-.003.001-.006.003-.023.009a12 12 0 0 1-.397.15c-.264.095-.631.223-1.047.35-.816.252-1.879.523-2.71.523-.847 0-1.548-.28-2.158-.525l-.028-.01C7.68 8.71 7.14 8.5 6.5 8.5c-.7 0-1.638.23-2.437.477A20 20 0 0 0 3 9.342V15.5a.5.5 0 0 1-1 0V.5a.5.5 0 0 1 1 0v.282c.226-.079.496-.17.79-.26C4.606.272 5.67 0 6.5 0c.84 0 1.524.277 2.121.519l.043.018C9.286.788 9.828 1 10.5 1c.7 0 1.638-.23 2.437-.477a20 20 0 0 0 1.349-.476l.019-.007.004-.002h.001" />
+        </svg>
+    )
+}
+
+export default function Chat({
+    socket,
+    setToast,
+}: {
+    socket: Socket<DefaultEventsMap, DefaultEventsMap>
+    setToast: Dispatch<SetStateAction<string | null>>
+}) {
     const [messages, setMessages] = useState<ReactNode[]>([])
+    const [message, setMessage] = useState('')
     const messageEnd = useRef(null)
 
     useEffect(() => {
@@ -14,6 +37,7 @@ export default function Chat({ socket }: { socket: Socket<DefaultEventsMap, Defa
     }, [messages])
 
     socket.on('receive message', (message: string) => {
+        console.log(message)
         setMessages((prevMessages) => [
             ...prevMessages,
             <li
@@ -25,12 +49,55 @@ export default function Chat({ socket }: { socket: Socket<DefaultEventsMap, Defa
             </li>,
         ])
     })
-    
-    socket.emit('')
+
+    function sendMessage() {
+        socket.emit('send message', message, ({ success, data }: { success: boolean; data: any }) => {
+            if (!success) {
+                console.log(data)
+                setToast(data)
+            } else {
+                setMessage('')
+            }
+        })
+    }
 
     return (
-        <div className="w-25 h-full">
-            <ul>{messages}</ul>
+        <div className="m-1 flex flex-grow flex-col p-5 md:p-0">
+            <div className="h-75"></div>
+            <div className="h-25">
+                <ul>{messages}</ul>
+                <form
+                    className="flex flex-row"
+                    onSubmit={(e) => {
+                        e.preventDefault()
+                        sendMessage()
+                    }}
+                >
+                    <TextInput
+                        type="text"
+                        className="w-full"
+                        style={{ borderStartEndRadius: 0, borderEndEndRadius: 0 }}
+                        value={message}
+                        placeholder="Send a message"
+                        onChange={(event) => {
+                            setMessage(event.target.value)
+                        }}
+                        onKeyDown={(event) => {
+                            if (!message.length) return
+
+                            event.key === 'Enter' && sendMessage()
+                        }}
+                    />
+                    <Button
+                        type="button"
+                        className="rounded-s-none"
+                        onClick={sendMessage}
+                        style={{ width: 'fit-content' }}
+                    >
+                        Send
+                    </Button>
+                </form>
+            </div>
         </div>
     )
 }
