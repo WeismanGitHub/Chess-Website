ee80b04d6f7af558452d0b0499ed947b77848d32
weisman <weisman123@outlook.com>
2024-11-16 13:22:51 -0800

    Improve background gradient

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,20 +21,20 @@ export default function RootLayout({
     children: React.ReactNode
 }>) {
     return (
-        <html lang="en">
-            <body className="bg-white">
-                {/* Background Gradient https://github.com/ibelick/background-snippets */}
-                <div className="flex min-h-screen flex-col">
+        <html lang="en" className="min-h-full">
+            <body className="min-h-full bg-white">
+                <div className="relative flex min-h-screen flex-col">
                     <header>
                         <Navbar />
                     </header>
+                    {/* Background Gradient */}
                     <div
                         aria-hidden="true"
-                        className="absolute top-0 z-[-2] h-screen w-screen bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(14,116,144,0.4),rgba(255,255,255,0))]"
+                        className="absolute inset-0 z-[-2] bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(14,116,144,0.4),rgba(255,255,255,0))]"
                     />
                     <div
                         aria-hidden="true"
-                        className="absolute top-0 z-[-2] h-screen w-screen rotate-180 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(14,116,144,0.4),rgba(255,255,255,0))]"
+                        className="absolute inset-0 z-[-3] rotate-180 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(14,116,144,0.4),rgba(255,255,255,0))]"
                     />
                     <main className={`${inter.className} flex flex-1 items-center justify-center`}>
                         {children}
