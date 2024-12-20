743250c55ce373e42c7ce1129f7dc012e70f8de7
weisman <weisman123@outlook.com>
2024-12-19 18:41:13 -0800

    Create getKingSquare method

--- src/lib/chess/board.ts
+++ src/lib/chess/board.ts
@@ -1,3 +1,5 @@
+import { Color } from '../../types'
+import { King } from './pieces'
 import Square from './square'
 
 export default class Board {
@@ -13,5 +15,20 @@ export default class Board {
         const square: Square | null = this.squares[row][col]
 
         return square
+    }
+
+    getKingSquare(color: Color): Square {
+        for (let i = 0; i < this.squares.length; i++) {
+            for (let j = 0; j < this.squares[i].length; j++) {
+                const square = this.squares[i][j]
+                const piece = square.piece
+
+                if (piece && piece instanceof King && piece.color === color) {
+                    return square
+                }
+            }
+        }
+
+        throw new Error(`Board does not have a ${color} king.`)
     }
 }
