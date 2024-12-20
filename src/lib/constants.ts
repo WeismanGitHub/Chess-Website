9a33aa628df530510120a9356d84a8a71679c5ea
weisman <weisman123@outlook.com>
2024-12-19 20:02:26 -0800

    Create LobbyConstants

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -5,3 +5,9 @@ export class UserConstants {
     public static readonly minPasswordLength = 10
 }
 
+export class LobbyConstants {
+    public static readonly idLength = 3
+
+    public static readonly minMinutesLength = 1
+    public static readonly maxMinutesLength = 240
+}
