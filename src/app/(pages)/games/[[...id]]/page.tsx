5964b826b75fdd65c07d465b7a93279ac1706b29
weisman <weisman123@outlook.com>
2025-01-07 16:32:59 -0800

    Auto connect to room if id is in url

--- src/app/(pages)/games/[[...id]]/page.tsx
+++ src/app/(pages)/games/[[...id]]/page.tsx
@@ -271,40 +271,6 @@ function RoomTabs({
     )
 }
 
-// function UserWidget({ time, name, ready }: { time: number; name: string | null; ready: boolean }) {
-//     return (
-//         <div className="w-full">
-//             <div className="left-0">
-//                 {name}
-//                 {ready ? (
-//                     <svg
-//                         xmlns="http://www.w3.org/2000/svg"
-//                         width="16"
-//                         height="16"
-//                         fill="currentColor"
-//                         className="bi bi-check-square-fill"
-//                         viewBox="0 0 16 16"
-//                     >
-//                         <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm10.03 4.97a.75.75 0 0 1 .011 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.75.75 0 0 1 1.08-.022z" />
-//                     </svg>
-//                 ) : (
-//                     <svg
-//                         xmlns="http://www.w3.org/2000/svg"
-//                         width="16"
-//                         height="16"
-//                         fill="currentColor"
-//                         className="bi bi-x-square-fill"
-//                         viewBox="0 0 16 16"
-//                     >
-//                         <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm3.354 4.646L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 1 1 .708-.708" />
-//                     </svg>
-//                 )}
-//             </div>
-//             <span className="right-0">{time}</span>
-//         </div>
-//     )
-// }
-
 function Game({ socket }: { socket: Socket<DefaultEventsMap, DefaultEventsMap> }) {
     const [size, setSize] = useState(0)
 
@@ -348,7 +314,23 @@ function Game({ socket }: { socket: Socket<DefaultEventsMap, DefaultEventsMap> }
 
 export default function () {
     const [socket, setSocket] = useState<Socket<DefaultEventsMap, DefaultEventsMap> | null>(null)
-    let id: string
+
+    const params = useParams<{ id: string }>()
+    const router = useRouter()
+
+    if (params.id) {
+        const socket = io()
+
+        socket.on('connect', () => {
+            socket.emit(JoinRoom.Name, params.id, (res: JoinRoom.Response) => {
+                if (res.success) {
+                    return setSocket(socket)
+                }
+
+                toaster.error(res.error)
+            })
+        })
+    }
 
     return (
         <div className="mx-auto flex h-full w-full flex-col items-center justify-center px-6 py-8 lg:py-0">
@@ -362,6 +344,7 @@ export default function () {
                         socket.on('connect', () => {
                             socket.emit(JoinRoom.Name, values.id, (res: JoinRoom.Response) => {
                                 if (res.success) {
+                                    router.push(`/games/${res.data}`)
                                     return setSocket(socket)
                                 }
 
@@ -375,8 +358,7 @@ export default function () {
                         socket.on('connect', () => {
                             socket.emit(CreateRoom.Name, values.minutes, (res: CreateRoom.Response) => {
                                 if (res.success) {
-                                    id = res.data
-                                    console.log(id)
+                                    router.push(`/games/${res.data}`)
                                     return setSocket(socket)
                                 }
 
