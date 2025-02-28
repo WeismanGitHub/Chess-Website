46d03941f0fdf5ef080f127443770fcc044ddf9b
weisman <weisman123@outlook.com>
2025-02-27 21:17:29 -0800

    Delete HalfMove

--- src/lib/chess/index.ts
+++ src/lib/chess/index.ts
@@ -1,7 +1,6 @@
-import HalfMove from './half-move'
 import Square from './square'
 import Board from './board'
 import Clock from './clock'
 import Game from './game'
 
-export { Game, Clock, Board, HalfMove, Square }
+export { Game, Clock, Board, Square }
